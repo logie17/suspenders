e63473eddf721d0159b3b5b648912d59653d2d8a
Logan Bell <loganbell@gmail.com>
2022-01-23 20:31:15 -0800

    Adding racket-xp-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -177,6 +177,7 @@
   (find-file "~/work/personal/.dotfiles/Bash.org"))
 
 (use-package racket-mode
+  :hook (racket-mode . racket-xp-mode)
   :ensure t)
 
 (defun logan/lsp-mode-setup ()
@@ -398,16 +399,3 @@
    (python . t )))
 
 (setq org-confirm-babel-evaluate nil)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(racket-mode which-key web-mode visual-fill-column use-package typescript-mode smart-tab rainbow-delimiters org-bullets lsp-ui lsp-treemacs lsp-ivy js2-mode ivy-rich helpful gnuplot-mode gnuplot general forge evil-collection doom-themes doom-modeline counsel-projectile company-box)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
